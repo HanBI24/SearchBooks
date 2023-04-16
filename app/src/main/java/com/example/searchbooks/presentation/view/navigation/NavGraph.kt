4c7dcd4e915897fe50f29990947ac54241e96d58
HanBI24 <alsltjdrhs@naver.com>
2023-04-17 05:36:02 +0900

    Add NavGraph

---
+++ app/src/main/java/com/example/searchbooks/presentation/view/navigation/NavGraph.kt
@@ -0,0 +1,48 @@
+package com.example.searchbooks.presentation.view.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import coil.annotation.ExperimentalCoilApi
+
+@ExperimentalCoilApi
+@Composable
+fun SetupNavGraph(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = NavigationScreen.Main.route,
+    ) {
+        composable(
+            route = NavigationScreen.Main.route,
+            arguments = listOf(navArgument(DEFAULT_ARGUMENT_KEY) {
+                type = NavType.StringType
+            },
+                navArgument(DEFAULT_ARGUMENT_KEY2) {
+                    type = NavType.BoolType
+                }
+            )
+        ) {
+            MainScreen(
+                navController,
+                it.arguments?.getString(DEFAULT_ARGUMENT_KEY) ?: "",
+                it.arguments?.getBoolean(DEFAULT_ARGUMENT_KEY2) ?: false
+            )
+        }
+        composable(
+            route = NavigationScreen.RecentSearchWord.route,
+        ) {
+            RecentSearchWordScreen(
+                modifier = Modifier
+                    .fillMaxSize(),
+                navController = navController
+            )
+        }
+    }
+}
