8ffaca6b2525df2c63925200581e95fb72822a33
HanBI24 <alsltjdrhs@naver.com>
2023-04-17 04:41:46 +0900

    Add Constants and Clean Architecture's structure

---
+++ app/src/main/java/com/example/searchbooks/common/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.searchbooks.common
+
+object Constants {
+    const val BASE_URL = "https://openapi.naver.com"
+
+    const val ITEM_PER_PAGE = 10
+    const val PREV_NEXT_ITEM_PAGE = 10
+}
