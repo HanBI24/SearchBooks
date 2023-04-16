4320cde3701a6300ab873398f4d8d62050e3463a
HanBI24 <alsltjdrhs@naver.com>
2023-04-17 05:18:35 +0900

    Add SearchBookLocal to SearchBookEntity to translate

--- app/src/main/java/com/example/searchbooks/data/mapper/Mapper.kt
+++ app/src/main/java/com/example/searchbooks/data/mapper/Mapper.kt
@@ -1,6 +1,8 @@
 package com.example.searchbooks.data.mapper
 
+import com.example.searchbooks.data.local.entity.SearchBookEntity
 import com.example.searchbooks.data.remote.dto.Item
+import com.example.searchbooks.domain.model.local.SearchBookLocal
 import com.example.searchbooks.domain.model.remote.SearchBookRemoteItem
 
 object Mapper {
@@ -13,5 +15,13 @@ object Mapper {
             publisher = publisher,
             title = title
         )
+    }
+
+    fun SearchBookEntity.toSearchBookLocal(): SearchBookLocal {
+        return SearchBookLocal(searchWord = searchWord)
+    }
+
+    fun SearchBookLocal.toSearchBookEntity(searchWord: String): SearchBookEntity {
+        return SearchBookEntity(searchWord = searchWord)
     }
 }
