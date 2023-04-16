a1cdd65c0af9e26556e6d53f730e632144a16066
HanBI24 <alsltjdrhs@naver.com>
2023-04-17 05:25:45 +0900

    Add insertSearchWord()

--- app/src/main/java/com/example/searchbooks/presentation/view/search_screen/viewmodel/SearchBookViewModel.kt
+++ app/src/main/java/com/example/searchbooks/presentation/view/search_screen/viewmodel/SearchBookViewModel.kt
@@ -8,17 +8,28 @@ import androidx.paging.cachedIn
 import com.example.searchbooks.common.Constants.ITEM_PER_PAGE
 import com.example.searchbooks.data.paging.SearchBookPagingSource
 import com.example.searchbooks.data.remote.api.SearchBookApi
+import com.example.searchbooks.domain.model.local.model.SearchBookLocal
+import com.example.searchbooks.domain.model.local.repository.SearchBookLocalRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchBookViewModel @Inject constructor(
-    private val searchBookApi: SearchBookApi
+    private val searchBookApi: SearchBookApi,
+    private val searchBookLocalRepository: SearchBookLocalRepository
 ) : ViewModel(){
 
     fun getSearchBookPagination(title: String) =
         Pager(PagingConfig(pageSize = ITEM_PER_PAGE)) {
             SearchBookPagingSource(searchBookApi, title)
         }.flow.cachedIn(viewModelScope)
+
+    suspend fun insertSearchWord(searchWord: String) {
+        viewModelScope.launch {
+            searchBookLocalRepository.insertSearchWord(
+                SearchBookLocal(searchWord = searchWord)
+            )
+        }
+    }
 }
