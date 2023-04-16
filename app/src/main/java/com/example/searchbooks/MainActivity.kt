683fec3bd07f6bde5442b3c52afe1126ceb26563
HanBI24 <alsltjdrhs@naver.com>
2023-04-17 05:40:27 +0900

    Setting to navController

--- app/src/main/java/com/example/searchbooks/MainActivity.kt
+++ app/src/main/java/com/example/searchbooks/MainActivity.kt
@@ -10,36 +10,23 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.searchbooks.presentation.view.navigation.SetupNavGraph
 import com.example.searchbooks.ui.theme.SearchBooksTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SearchBooksTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SearchBooksTheme {
-        Greeting("Android")
     }
 }
