7d087b6e29bee716fd4bb30f99d95badda1a4ce1
HanBI24 <alsltjdrhs@naver.com>
2023-04-17 04:53:08 +0900

    Add SearchBookPagingSource

--- app/src/main/java/com/example/searchbooks/data/mapper/Mapper.kt
+++ app/src/main/java/com/example/searchbooks/data/mapper/Mapper.kt
@@ -3,7 +3,6 @@ package com.example.searchbooks.data.mapper
 import com.example.searchbooks.data.remote.dto.Item
 import com.example.searchbooks.domain.model.remote.SearchBookRemoteItem
 
-
 object Mapper {
     fun Item.toSearchBookItem(): SearchBookRemoteItem {
         return SearchBookRemoteItem(
---
+++ app/src/main/java/com/example/searchbooks/data/paging/SearchBookPagingSource.kt
@@ -0,0 +1,42 @@
+package com.example.searchbooks.data.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.searchbooks.common.Constants.ITEM_PER_PAGE
+import com.example.searchbooks.common.Constants.PREV_NEXT_ITEM_PAGE
+import com.example.searchbooks.data.mapper.Mapper.toSearchBookItem
+import com.example.searchbooks.data.remote.api.SearchBookApi
+import com.example.searchbooks.domain.model.remote.SearchBookRemoteItem
+import javax.inject.Inject
+
+class SearchBookPagingSource @Inject constructor(
+    private val searchBookApi: SearchBookApi,
+    private val title: String
+) : PagingSource<Int, SearchBookRemoteItem>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SearchBookRemoteItem> {
+        return try {
+            val page = params.key ?: 1
+            val searchBookResponse = searchBookApi
+                .getSearchBook(title, ITEM_PER_PAGE, page)
+                .items.map {
+                    it.toSearchBookItem()
+                }
+
+            LoadResult.Page(
+                data = searchBookResponse,
+                prevKey = if (page == 1) null else page.minus(PREV_NEXT_ITEM_PAGE),
+                nextKey = page.plus(PREV_NEXT_ITEM_PAGE)
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, SearchBookRemoteItem>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+}
